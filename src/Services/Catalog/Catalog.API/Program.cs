31ec8da9571c43faeb6269def65d65f548a64c89
zurarev <zura1976@gmail.com>
2022-03-19 22:49:50 +0400

    commit basket nicroservice

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,31 @@
+using Catalog.API.Data;
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Repositories;
+using Catalog.API.Repositories.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>   endpoints.MapControllers());    
+
+app.Run();
